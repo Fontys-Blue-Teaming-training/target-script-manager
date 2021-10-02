4295e4506be2554698937716878b518940695707
SieBrum <siebrenkraak@hotmail.com>
2021-10-02 15:37:55 +0200

    Setup SocketClient usage example for Ivo

---
+++ TargetScriptManager/TargetScriptManager/TargetScriptManager/TargetScriptMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using websocket_client;
+
+namespace TargetScriptManager
+{
+    public class TargetScriptMessageHandler<T> : MessageHandler<T>
+    {
+        public override void HandleMessage(string message)
+        {
+            var obj = ReceiveAsObj(message);
+            throw new NotImplementedException();
+        }
+    }
+}
